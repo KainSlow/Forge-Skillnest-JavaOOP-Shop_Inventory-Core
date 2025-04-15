3e4541016d0d1630d824f489621349a1c0b50b1d
KainSlow <jorge_alfonso3@hotmail.com>
2025-04-15 14:06:06 -0400

    Added: Televisor.java subclass of ProductoElecetrodomestico.java

---
+++ com/clases/Televisor.java
@@ -0,0 +1,20 @@
+package com.clases;
+
+public class Televisor extends ProductoElectrodomestico{
+
+    private double tamañoPantalla; // diagonal/pulgadas
+    private String resolucion; //Ejemplo: 4:3, 16:9
+
+    public Televisor(String nombre, double precio, int cantidadDisponible, double tamañoPantalla, String resolucion) {
+        super(nombre, precio, cantidadDisponible);
+        this.tamañoPantalla = tamañoPantalla;
+        this.resolucion = resolucion;
+    }
+
+    @Override
+    public void mostrarInformacion(){
+        super.mostrarInformacion();
+        System.out.println("\tTamaño pantalla: " + String.valueOf(tamañoPantalla) + '"');
+        System.out.println("\tResolución: " + resolucion);
+    }
+}
